baa3710d35225c4a4712e589ef1b10268bb4f414
Francis Mujakachi <fmujakachi@hotmail.co.uk>
2024-06-18 09:38:04 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// Variables of different data types
+let str = "Hello";
+let num = 10;
+let bool = true;
+
+// Functions for simple operations
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// Calling the functions
+let resultAdd = add(5, 3);
+console.log("Addition:", resultAdd);
+
+let resultSubtract = subtract(10, 4);
+console.log("Subtraction:", resultSubtract);
+
+let resultDivide = divide(15, 3);
+console.log("Division:", resultDivide);
+
+let resultMultiply = multiply(6, 7);
+console.log("Multiplication:", resultMultiply);
